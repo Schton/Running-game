a249dbea6638f6d3d5692428c0b3c3a8f61f5d64
Mart Barbon <martbarbon8@gmail.com>
2026-03-14 18:17:31 +0800

    Added pause menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,12 @@
-extends Control
+extends CanvasLayer
+
+func _ready():
+	# Force the mouse to be visible and usable
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_play_button_pressed():
 	# Replace "res://scenes/level1.tscn" with the actual path to your level 1 scene
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
-
 
 func _on_quit_button_pressed():
 	# This closes the game
